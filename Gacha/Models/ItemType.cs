443acafd9b628b6123e0fc91d26189f9ccce781e
Bingsong Yu <bing.ybs@outlook.com>
2026-02-17 14:24:56 -0500

    gacha

---
+++ Gacha/Models/ItemType.cs
@@ -0,0 +1,19 @@
+﻿namespace Gacha.Models
+{
+    public enum ItemRarity
+    {
+        None = 0,           //default to 0
+        Common,             //0
+        Rare,               //1
+        Epic,               //2
+        Mystic,             //3 
+        Legendary           //4
+    }
+    public class ItemType
+    {
+        public int Id { get; set; }//primary key
+        public required string ItemName { get; set; }
+        public string? Description { get; set; }
+        public ItemRarity ItemRarity { get; set; }
+    }
+}
