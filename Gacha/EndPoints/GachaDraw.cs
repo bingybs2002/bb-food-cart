b0eeab9c10fe66f4aee30d4e184db06e8e00663b
Bingsong Yu <bing.ybs@outlook.com>
2026-02-25 16:29:18 -0500

    gacha

--- Gacha/EndPoints/GachaDraw.cs
+++ Gacha/EndPoints/GachaDraw.cs
@@ -1,4 +1,6 @@
 ﻿using Gacha.Data;
+using System.Linq;
+using Gacha.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Gacha.EndPoints
@@ -11,12 +13,13 @@ namespace Gacha.EndPoints
             //Get by drawing
             group.MapGet("/", async (AppDbContext db) =>
             {
-                var Count = await db.Items.CountAsync();
-                Console.Out.WriteLine("Numaber of Counts: " + Count);
+                var count = await db.Items.CountAsync();
+                Console.Out.WriteLine("Numaber of Counts: " + count);
 
-                var Index = Random.Shared.Next(1,Count+1);
-                Console.Out.WriteLine("Random Generated: " + Index);
-                return Results.Ok(Index);
+                var index = Random.Shared.Next(1,count+1);
+                var result = db.Items.FirstOrDefault(x=>x.Id == index);
+                Console.Out.WriteLine("Random Generated: " + index);
+                return Results.Ok(result);
             });
         }
     }
